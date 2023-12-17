91acd03228609a0241b7bf0a9b0296cb3b4d8b76
Meister <meistercodes@gmail.com>
2023-12-17 11:32:19 -0500

    repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,10 @@
-const repeatString = function () {};
+const repeatString = function (string, times) {
+  let repeatedString = '';
+  for (let i = 0; i < times; i++) {
+    repeatString += string;
+  }
+  return repeatedString;
+};
 
 // Do not edit below this line
 module.exports = repeatString;
