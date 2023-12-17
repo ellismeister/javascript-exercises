527e52281519e546d617d9ae228b9a2466adc9c5
Meister <meistercodes@gmail.com>
2023-12-17 11:28:09 -0500

    tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,12 +1,6 @@
-const convertToCelsius = function (temp) {
-  const convertedTemp = ((temp - 32) * 5) / 9;
-  return Math.round(convertedTemp * 10) / 10;
-};
+const convertToCelsius = function () {};
 
-const convertToFahrenheit = function (temp) {
-  const convertedTemp = (temp * 9) / 5 + 32;
-  return Math.round(convertedTemp * 10) / 10;
-};
+const convertToFahrenheit = function () {};
 
 // Do not edit below this line
 module.exports = {
