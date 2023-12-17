0c27f64a633285c89f8ad681b9d6f242c6ed5fdb
Meister <meistercodes@gmail.com>
2023-12-17 11:40:25 -0500

    leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,6 @@
-const leapYears = function () {};
+const leapYears = function (years) {
+  return years % 4 === 0 && (years % 100 !== 0 || years % 400 === 0);
+};
 
 // Do not edit below this line
 module.exports = leapYears;
