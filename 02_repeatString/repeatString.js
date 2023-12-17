263bc08ee84f674a5bd350d0cd0bfda5eef90fe2
Meister <meistercodes@gmail.com>
2023-12-17 11:33:59 -0500

    repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,7 +1,8 @@
 const repeatString = function (string, times) {
+  if (times < 0) return 'ERROR';
   let repeatedString = '';
   for (let i = 0; i < times; i++) {
-    repeatString += string;
+    repeatedString += string;
   }
   return repeatedString;
 };
